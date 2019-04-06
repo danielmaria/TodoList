d4bb36ceb68551b50896bc983b8860757b3d63d4
Daniel Maria da Silva <danielmariadasilva@gmail.com>
2019-04-06 12:36:48 -0300

    Added profiles to the application

--- src/main/java/br/com/todolist/TodoListApplication.java
+++ src/main/java/br/com/todolist/TodoListApplication.java
@@ -1,13 +1,22 @@
 package br.com.todolist;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Project is running ###");
+		};
 	}
 
 }
